9bbb6f71ce4b5445c9fe5d9169c65e23aff8abdf
Brendan Kormos <brendaaank@gmail.com>
2023-11-15 00:33:04 +0000

    toggleEntries function  created, viewSnap function created and tested

--- js/data.js
+++ js/data.js
@@ -6,9 +6,15 @@ const $notes = document.querySelector('#notes');
 const tempIMG = $img.src;
 const $templateEntry = document.querySelector('.list-item-entry');
 const $entriesUnorderedList = document.querySelector('#entries-unordered-list');
+const $noEntriesAlert = document.querySelector('#no-entries-alert');
+
+const views$ = {
+  entries: document.querySelector('[data-view=entries]'),
+  'entry-form': document.querySelector('[data-view=entry-form]'),
+};
 
 let data = {
-  view: 'entry-form',
+  view: 'entries',
   entries: [],
   editing: null,
   nextEntryId: 1,
@@ -48,9 +54,29 @@ window.addEventListener('beforeunload', function (event) {
 const oldData = localStorage.getItem('first-code-journal');
 if (oldData !== null) data = JSON.parse(oldData);
 
+function viewSnap(viewName /* ("entries" | "entry-form") */) {
+  views$[data.view].classList.add('hidden');
+  data.view = viewName;
+  views$[viewName].classList.remove('hidden');
+}
+
+let noEntries = true;
+// eslint-disable-next-line no-unused-vars
+function toggleNoEntries() {
+  if (noEntries) $noEntriesAlert.classList.remove('hidden');
+  else $noEntriesAlert.classList.add('hidden');
+  noEntries = !noEntries;
+}
+
 document.addEventListener('DOMContentLoaded', function (event) {
   for (let i = 0; i < data.entries.length; i++) {
     const $entry = renderEntry(data.entries[i]);
     $entriesUnorderedList.prepend($entry);
   }
+
+  // reset views prior to setting
+  for (const key in views$) {
+    views$[key].classList.add('hidden');
+  }
+  viewSnap(data.view);
 });
